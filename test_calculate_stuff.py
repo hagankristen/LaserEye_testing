3989e3e1ec9cce652cbae0fe6d07de5bbe54972f
hagankristen <kristenhagan@DESKTOP-F6L9VU9.localdomain>
2018-02-20 21:04:01 -0500

    new module changes

--- test_calculate_stuff.py
+++ test_calculate_stuff.py
@@ -1,60 +1,39 @@
 import numpy as np
 import pytest
+from calculate_stuff import CalculateStuff
 
+def test_calculate_stuff:
 
-def test_max_diff():
+    alist1 = CalculateStuff([1, 2, 3, 4, 5])
+    assert(np.isclose(alist1.sum_nums(), 15))
+    assert(np.isclose(alist1.max_diff(), 1))
+    assert(alist1.min_max() == (1, 5))
 
-    from calculate_stuff import max_diff
-    import numpy as np
-    import pytest
+    alist2 = CalculateStuff([9.0, 1.5, 3.2])
+    assert(np.isclose(alist2.sum_nums(), 13.7))
+    assert(np.isclose(alist2.max_diff(), 7.5))
+    assert(alist2.min_max() == (1.5, 9.0))
 
-    max_value1 = max_diff([1, 2, 3, 4, 5])
-    max_value2 = max_diff([9.0, 1.5, 3.2])
-    max_value3 = max_diff([-5, -7, 5, -2, 12, 15])
+    alist3 = CalculateStuff([-5, -7, 5, -2, 12, 15])
+    assert(np.isclose(alist3.sum_nums(), 18))
+    assert(np.isclose(alist3.max_diff(), 14))
+    assert(alist3.min_max() == (-7, 15))
 
-    assert(np.isclose(max_value1, 1))
-    assert(np.isclose(max_value2, 7.5))
-    assert(np.isclose(max_value3, 14))
-
+    alist4 = CalculateStuff([9,'h', 8])
     with pytest.raises(TypeError):
-        max_diff(['a', 'b', 'c'])
-
-    with pytest.raises(ValueError):
-        max_diff([7])
-
-    with pytest.raises(ValueError):
-        max_diff([4, 5, float('nan')])
-
-
-def test_min_max():
-
-    from calculate_stuff import min_max
-
-    min_max_val1 = min_max([1, 2, 3, 4, 5])
-    min_max_val2 = min_max([-1, -3, -5, 4, 2, 0])
-    min_max_val3 = min_max([-6.2, -5, -3, -1, 1.4, 1.4, 3])
-
-    assert(min_max_val1 == (1, 5))
-    assert(min_max_val2 == (-5, 4))
-    assert(min_max_val3 == (-6.2, 3))
+        alist4.max_diff()
+    with pytest.raises(TypeError):
+        alist4.sum_nums()
+    with pytest.raises(TypeError):
+        alist4.min_max()
 
     with pytest.raises(ImportError):
-        import Number
+        import random
 
-    with pytest.raises(TypeError):
-        min_max([1, 'b', 'c'])
-
+    alist5 = CalculateStuff([])
     with pytest.raises(ValueError):
-        min_max([])
-
-
-def test_sum_nums():
-    from calculate_stuff import sum_nums
-
-    sum_1 = sum_nums([-1, 9.8, 7])
-    sum_2 = sum_nums([-10, 0.5, 0.5, 5.5])
-    sum_3 = sum_nums([2, 5, 7, 8, 1])
-
-    assert(np.isclose(sum_1, 15.8))
-    assert(np.isclose(sum_2, -3.5))
-    assert(np.isclose(sum_3, 23))
+        alist5.sum_nums();
+    with pytest.raises(ValueError):
+        alist5.max_diff();
+    with pytest.raises(ValueError):
+        alist5.min_max();        
